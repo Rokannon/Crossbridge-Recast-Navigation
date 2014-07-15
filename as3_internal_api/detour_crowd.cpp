ea41ea2cb3f1f270794c31e82e78f277edd4e05b
Vladimir Atamanov <rokannon@gmail.com>
2014-07-16 00:28:23 +0400

    Added empty 'detour_crowd.cpp' to Visual Studio project.

---
+++ as3_internal_api/detour_crowd.cpp
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <AS3\AS3.h>
+#include <AS3\AS3++.h>
+#include <DetourCrowd.h>
+#include <DetourLocalBoundary.h>
+#include <DetourObstacleAvoidance.h>
+#include <DetourPathCorridor.h>
+#include <DetourPathQueue.h>
+#include <DetourProximityGrid.h>
+
