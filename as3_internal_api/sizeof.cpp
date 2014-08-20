4d8c144b4bbba5f736a9c6270217eba073fe1c9b
Vladimir Atamanov <rokannon@gmail.com>
2014-08-20 20:24:02 +0400

    Added more internal_sizeof_ methods to internal API.

--- as3_internal_api/sizeof.cpp
+++ as3_internal_api/sizeof.cpp
@@ -35,14 +35,55 @@ void _sizeof_ ## x()\
 	AS3_Return(sizeof(x));\
 }
 
+// ////
+// Recast
+// ////
+
+_SIZEOF(rcContour);
+_SIZEOF(rcContourSet);
+_SIZEOF(rcPolyMesh);
+_SIZEOF(rcPolyMeshDetail);
+_SIZEOF(rcCompactCell);
+_SIZEOF(rcCompactHeightfield);
+_SIZEOF(rcCompactSpan);
+_SIZEOF(rcConfig);
+_SIZEOF(rcContext);
+_SIZEOF(rcHeightfield);
+_SIZEOF(rcHeightfieldLayer);
+_SIZEOF(rcHeightfieldLayerSet);
+_SIZEOF(rcSpan);
+_SIZEOF(rcSpanPool);
+
+// ////
+// Detour
+// ////
+
+_SIZEOF(dtBVNode);
+_SIZEOF(dtLink);
+_SIZEOF(dtMeshHeader);
+_SIZEOF(dtMeshTile);
+_SIZEOF(dtNavMesh);
+_SIZEOF(dtNavMeshParams);
+_SIZEOF(dtOffMeshConnection);
+_SIZEOF(dtPoly);
+_SIZEOF(dtPolyDetail);
+_SIZEOF(dtNavMeshCreateParams);
+_SIZEOF(dtNavMeshQuery);
+_SIZEOF(dtQueryFilter);
+_SIZEOF(dtRaycastHit);
+
+// ////
+// Detour Crowd
+// ////
+
+_SIZEOF(dtCrowd);
+_SIZEOF(dtCrowdAgent);
+_SIZEOF(dtCrowdAgentDebugInfo);
+_SIZEOF(dtCrowdAgentParams);
+_SIZEOF(dtCrowdNeighbour);
+_SIZEOF(dtObstacleAvoidanceParams);
+
 _SIZEOF(dtPathCorridor);
 _SIZEOF(dtLocalBoundary);
 _SIZEOF(dtPathQueue);
-_SIZEOF(dtQueryFilter);
-
-_SIZEOF(dtCrowdNeighbour);
-_SIZEOF(dtCrowdAgentParams);
-_SIZEOF(dtCrowdAgent);
 _SIZEOF(dtObstacleAvoidanceDebugData);
-_SIZEOF(dtCrowd);
-_SIZEOF(dtObstacleAvoidanceParams);
